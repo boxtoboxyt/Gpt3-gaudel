a17a7bd2e1b478685aa0fd23d51388568cd96321
Romain Gaudel <romaingaudel83@gmail.com>
2023-01-03 11:42:08 +0100

    Ajout style general

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './App.css'
+
 import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers';
 import { Brand, Cta, Navbar } from './components';
 
