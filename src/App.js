94b807ab69b3790f5ba00349c808104a21c3125d
Romain Gaudel <romaingaudel83@gmail.com>
2023-01-03 12:14:20 +0100

    assets + debut style navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from 'react';
 
-import './App.css'
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { Cta, Brand, Navbar } from './components';
 
-import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers';
-import { Brand, Cta, Navbar } from './components';
+import './App.css';
 
-
-function App() {
-  return (
-    <div className='App'>
-        <div className='gradient_bg'>
-            <Navbar />
-            <Header />
-        </div>
-        <Brand />
-        <WhatGPT3 />
-        <Features />
-        <Possibility />
-        <Cta />
-        <Blog />
-        <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <Cta />
+    <Blog />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
