5d0f6383c05bfd035d8437cb8fda3ffbfef692cc
Romain Gaudel <romaingaudel83@gmail.com>
2023-01-03 11:33:35 +0100

    Structure Website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, Header, WhatGPT3 } from './containers';
+import { Brand, Cta, Navbar } from './components';
+
 
 function App() {
   return (
-    <div>GPT-3</div>
+    <div className='App'>
+        <div className='gradient_bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+    </div>
   )
 }
 
